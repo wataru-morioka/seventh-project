976fe501abf9c54354f17b8b94fe6264e2fa4adb
WataruMorioka <wtr_morioka@icloud.com>
2019-07-27 09:29:27 +0900

    postgres追加

--- flask/src/run.py
+++ flask/src/run.py
@@ -1,10 +1,28 @@
 # coding: utf-8
-from flask import Flask, abort, request
+from flask import Flask, abort, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from sqlalchemy import func
+import os
+
+POSTGRES_URL = os.environ["POSTGRES_URL"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PASSWORD,url=POSTGRES_URL,db=POSTGRES_DB)
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+ma = Marshmallow(app)
 api = Api(app)
 
 # テストデータ
@@ -14,7 +32,22 @@ users = [
     { "id": "U003", "name": "ユーザ三郎", "age": 10 }
 ]
 
-class User(Resource):
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(200), unique=False, nullable=True)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.token = name
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'token')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+class UserInfo(Resource):
     def get(self):
         """
         ユーザを１件取得する
@@ -22,7 +55,15 @@ class User(Resource):
         # id = request.args.get('id')
         # result = [n for n in users if n["id"] == id]
 
-        return users[0]
+        # user = User(2, 'wataru')
+
+        # db.session.add(user)
+
+        # db.session.commit()
+        user = User.query.filter_by(id = 2).first()
+
+        return jsonify(user_schema.dump(user).data)
+        # return users[0]
 
         # if len(result) >= 1: 
         #     # ユーザ情報を返却
@@ -74,8 +115,7 @@ class User(Resource):
             #存在しないユーザIDが指定された場合
             abort(404)
 
-api.add_resource(User, '/user')
+api.add_resource(UserInfo, '/user')
 
 if __name__ == "__main__":
-    print("サーバ起動")
     app.run(debug=True)
--- nginx.dockerfile
+++ nginx.dockerfile
@@ -4,14 +4,13 @@ RUN useradd -m -s /bin/bash -u 1000 wataru
 
 RUN sed -i 's/user\ \ nginx\;/user\ \ wataru\;/g' /etc/nginx/nginx.conf
 
-RUN apt-get update && apt-get install -y vim
-RUN apt-get install -y curl
-RUN apt-get install -y procps
+RUN apt-get update && apt-get install -y vim && \
+    apt-get install -y curl && \
+    apt-get install -y procps && \
+    mkdir /var/log/nginx/site
 
 # RUN /bin/cp -f /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 ADD ./uwsgi_params /etc/nginx/uwsgi_params
-
-#ADD ./vhost.conf /etc/nginx/conf.d/default.conf
-#CMD ["tail","-f","dev/null"]
+ADD ./vhost.conf /etc/nginx/conf.d/default.conf
 
 WORKDIR /var/www
